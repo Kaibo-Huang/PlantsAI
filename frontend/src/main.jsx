3bf61e6c44b9c0e5d9f0dd0285fea55f39fc0caa
chang <chang@chang02.local>
2025-07-19 09:43:14 -0400

    x

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider
-    domain="dev-cr6jw6a7bnbauopb.us.auth0.com"
-    clientId="LGSqfK6my7gT8IuaWnYr1JvS9EcBJW5F"
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
-    <App />
-  </Auth0Provider>
+  <App />
 );
