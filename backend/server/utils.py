5254b77e8e181c7085ea4d9b780a5ae019340772
Coco Zeng <cokeymint@gmail.com>
2025-07-19 09:04:22 -0400

    Update utils.py

--- backend/server/utils.py
+++ backend/server/utils.py
@@ -34,6 +34,38 @@ def get_weather_data(lat, lon):
     api_endpoint = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_key}"
     return requests.get(api_endpoint).json()
 
+def add_plant_data(plant_data):
+    client = MongoClient("mongodb+srv://stringbot:u2ZG9kM5q8L0WaNW@plantmap.ipx3g1d.mongodb.net/")
+    db = client["plantmap"]
+    detections = db["detections"]
+    detections.insert_one(plant_data)
+    # example
+    # detections.insert_one({
+    #   "type": "Feature",
+    #   "geometry": {
+    #     "type": "Point",
+    #     "coordinates": [125.6, 10.1]
+    #   },
+    #   "properties": {
+    #     "name": "Japanese Knotweed",
+    #     "confidence": 0.94,
+    #     "timestamp": "2025-07-18T22:00:00Z",
+    #     "imageUrl": "/detections/123.jpg",
+    #     "weather": {
+    #       "temperature": 26.1,
+    #       "humidity": 78
+    #     },
+    #     "soil": {
+    #       "moisture": 0.54,
+    #       "nitrogen": 13.2
+    #     }
+    #   }
+    # }
+    # )
+
+def view_data():
+    return jsonify(list(db.detections.find({}, {"_id": 0})))
+
 if __name__ == "__main__":
     file_paths = [r"C:\Users\yroeh\Documents\Github\plant-map\server\test_img.jpg"]
     plant_data = get_plantnet_data(file_paths)
